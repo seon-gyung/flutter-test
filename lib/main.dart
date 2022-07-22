67557d349c0f05650e0ee4c25b1400e7b6da9b13
seongyung <282203@naver.com>
2022-07-22 09:07:47 +0900

    메인 페이지에서 다른 페이지 가기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:app_test1/testpages/second.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,64 +12,42 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const GetMaterialApp(
+      home: FirstWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+class FirstWidget extends StatelessWidget {
+  const FirstWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: const Text(
+          "선경앱",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+          ),
         ),
+        backgroundColor: Colors.indigoAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Row(children: [
+        GestureDetector(
+          onTap: (){
+            Get.to(second());
+          },
+          child: Container(
+            child: Text(
+              "다른 페이지 가기",
+              style: TextStyle(fontSize: 30),
+            ),
+            color: Colors.cyanAccent,
+          ),
+        ),
+      ]),
     );
   }
 }
